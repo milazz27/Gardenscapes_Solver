dd5b0be95ae1a6f86618457fb3f9e84f077cf01d
Mila <milatila@milaspc.localdomain>
2025-12-09 16:36:36 -0500

    cleaned up files

--- src/grid.pl
+++ src/grid.pl
@@ -40,51 +40,7 @@ none(empty, pos(Row, Col)).
 %-------------------------------------------------------------------------------------------%
 
 get_row_col(pos(R, C), R, C).
-
 get_row(pos(R, _), R).
-
 get_col(pos(_,C), C).
 
-%-------------------------------------------------------------------------------------------%
-% Definition for Adjacency:
-%-------------------------------------------------------------------------------------------%
-
-% check right
-adjacent(R1, C1, R2, C2) :-
-    Right is R1 + 1,
-    R2 =:= Right.
-
-% check below
-adjacent(R1, C1, R2, C2) :-
-    Below is C1 + 1,
-    C2 =:= Below.
-
-% check left
-adjacent(R1, C1, R2, C2) :-
-    Left is R1 - 1,
-    R2 =:= Left.
-
-% check above
-adjacent(R1, C1, R2, C2) :-
-    Above is C1 - 1,
-    C2 =:= Above.
-
-%-------------------------------------------------------------------------------------------%
-% Definition for Valid Swaps:
-%-------------------------------------------------------------------------------------------%
-
-unswappable(P1, P2) :- 
-    is_empty(P1),
-    is_empty(P2),
-    !.
-
-unswappable(P1, P2) :- 
-    is_object(P1),
-    is_object(P2).
-
-valid_swap(R1, C1, R2, C2) :-
-    piece(Type1, R1, C1),
-    piece(Type2, R2, C2),
-    \+ unswappable(Type1, Type2),
-    adjacent(R1, C1, R2, C2).
 
--- src/grid_ops.pl
+++ src/grid_ops.pl
@@ -2,6 +2,64 @@
 :- ['grid.pl'].
 :- use_module(library(http/json)).
 
+%===========================================================================================%
+%  Definition for Adjacency:                                                                %
+%===========================================================================================%
+
+% check right 
+adjacent(Pos1, Pos2) :-
+    get_row_col(Pos1, Row1, Col1),
+    get_row_col(Pos2, Row2, Col2),
+    Row1 =:= Row2,
+    Col2 =:= Col1 + 1.
+
+% check left
+adjacent(Pos1, Pos2) :-
+    get_row_col(Pos1, Row1, Col1),
+    get_row_col(Pos2, Row2, Col2),
+    Row1 =:= Row2,
+    Col1 =:= Col2 + 1.
+
+% check up
+adjacent(Pos1, Pos2) :-
+    get_row_col(Pos1, Row1, Col1),
+    get_row_col(Pos2, Row2, Col2),
+    Col1 =:= Col2,
+    Row2 =:= Row1 + 1.
+
+%check down
+adjacent(Pos1, Pos2) :-
+    get_row_col(Pos1, Row1, Col1),
+    get_row_col(Pos2, Row2, Col2),
+    Col1 =:= Col2,
+    Row1 =:= Row2 + 1.
+
+%-------------------------------------------------------------------------------------------%
+% Definition for Valid Swaps:
+%-------------------------------------------------------------------------------------------%
+% -- takes in type cell(type(name), pos(r, c))
+
+% Cannot swap 2 empty cells
+unswappable(P1, P2) :- 
+    is_empty(P1),
+    is_empty(P2),
+    !.
+
+unswappable(P1, P2) :- 
+    is_object(P1),
+    is_object(P2).
+
+valid_swap(R1, C1, R2, C2) :-
+    piece(Type1, R1, C1),
+    piece(Type2, R2, C2),
+    \+ unswappable(Type1, Type2),
+    adjacent(R1, C1, R2, C2).
+
+
+
+
+
+
 initial_state(State) :-
     findall(piece(Type, Row, Col), piece(Type, Row, Col), State).
 
